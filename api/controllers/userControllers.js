83d89926920ac38fdc95e4b81b51a2d2542add68
ala-boutabia <alahaya36@gmail.com>
2024-01-17 00:01:40 +0100

    Test api route

---
+++ api/controllers/userControllers.js
@@ -0,0 +1,5 @@
+export const getAllUsers = (req, res) => {
+  res.json({
+    message: "Get All Users",
+  });
+};
--- api/server.js
+++ api/server.js
@@ -1,8 +1,25 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes.js";
+dotenv.config();
+
+//Connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to db");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 const app = express();
 
-const Port = 3000;
+// Routes
+app.use("/api/users", userRoutes);
+
+const Port = process.env.PORT || 3500;
 app.listen(Port, () => {
-  console.log(`Server is running on ${Port}`);
+  console.log(`Server is running on port: ${Port}`);
 });
